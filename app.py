2b2f80b5601dfa163d19512109ba61c8d2116d8a
Liveserver01 <virendrachauhan11234@gmail.com>
2025-07-20 23:18:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,4 +19,5 @@ def get_joke():
     return jsonify({"joke": joke})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
